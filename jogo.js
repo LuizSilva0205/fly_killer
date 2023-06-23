42840a80f138bb454404538b7c2a2bf3acd8c54e
LUIZ SILVA <117494851+LuizSilva0205@users.noreply.github.com>
2023-06-22 22:29:04 -0300

    up

--- jogo.js
+++ jogo.js
@@ -4,8 +4,20 @@ var altura = 0
 var largura = 0
 
 var vidas = 1
-
 var tempo = 10
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if( nivel === 'normal'){
+    criaMosquitoTempo = 1500
+}else if(nivel === 'dificil'){
+    criaMosquitoTempo = 1000
+}else if(nivel === 'chucknorris'){
+    criaMosquitoTempo = 750
+}
 
 
 
