1bf2d14a8ff2192a9052a4c751f9649d297eba20
LUIZ SILVA <117494851+LuizSilva0205@users.noreply.github.com>
2023-06-22 21:59:54 -0300

    up

--- jogo.js
+++ jogo.js
@@ -3,6 +3,10 @@
 var altura = 0
 var largura = 0
 
+var vidas = 1
+
+var tempo = 10
+
 
 
 // recuperando altura e largura da janela
@@ -16,9 +20,38 @@ function ajustaTamanhoPalcoJogo(){
 ajustaTamanhoPalcoJogo()
 
 
+var cronometro = setInterval(function(){
+    tempo -=1
+    if(tempo < 0){
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = 'vitoria.html'
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+   
+},1000)
+
 //variaveis do mosquito
 
 function posicaoRandomica(){
+
+    //remover mosquito anterior caso exista
+
+    if( document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+
+        if( vidas>3){
+            window.location.href = 'fim_de_jogo.html'
+        }else{
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+            vidas++
+        }
+      
+    }
+    
+
+
     var posicaoX = Math.floor(Math.random() * largura)-90
     var posicaoY = Math.floor(Math.random() * altura)-90
 
@@ -32,13 +65,18 @@ function posicaoRandomica(){
     
     var mosquito = document.createElement('img')
     mosquito.src= 'imagens/mosca.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio() + ' ' +ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
     
     document.body.appendChild(mosquito)
-    tamanhoAleatorio()    
+    console.log(ladoAleatorio())
+       
 }
 
 
@@ -54,4 +92,14 @@ function tamanhoAleatorio(){
     }
 }
 
+function ladoAleatorio(){
+    var classe =Math.floor(Math.random() * 2)
+    switch(classe){
+        case 0:
+            return'ladoA'
+        case 1:
+            return'ladoB'
+
+    }
+}
 
